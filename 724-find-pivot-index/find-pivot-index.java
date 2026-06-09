1e069345ad6a07a607545ec432ec986aa94934c0
Aditi Kumari <kaditi876@gmail.com>
2026-06-09 13:29:53 +0530

    Time: 1 ms (98.43%) | Memory: 47.2 MB (87.29%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int sum=0;
+        int right_sum=0;
+        int left_sum=0;
+        for (int num:nums){
+            sum+=num;
+        }
+        for (int i=0;i<nums.length;i++){
+            right_sum=sum-left_sum-nums[i];
+            if (right_sum==left_sum){
+                return i;
+            }
+            left_sum+=nums[i];
+        }
+        return -1;
+    }
+}
