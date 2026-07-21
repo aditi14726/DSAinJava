c5077ed9a5ce6b2dfa98dcc1783787d66c69cbb2
Aditi Kumari <kaditi876@gmail.com>
2026-07-21 13:15:12 +0530

    Time: 10 ms (31.40%) | Memory: 43.1 MB (62.58%) - LeetSync

---
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        int time=0;
+        for(int i=0; i < tickets.length; i++){
+            queue.offer(i);
+        }
+        while (!queue.isEmpty()){
+            int person=queue.poll();
+            tickets[person]--;
+            time++;
+            if(tickets[person] == 0 && person == k){
+                return time;
+        }
+            if (tickets[person] > 0){
+                queue.offer(person);
+        }
+        }
+        return -1;
+    }
+}
